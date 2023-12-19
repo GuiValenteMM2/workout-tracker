c67f0f4fedd294885e4c0fdd3b880f94d40c9e3b
GuiValenteMM2 <guimartins.m.v@gmail.com>
2023-12-19 13:25:18 -0300

    dependencies setup done

---
+++ workout-tracker/src/components/Header.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <div class="container bg-red-600"> 
+    <div class="">Workout Tracker</div>
+    <nav class="container no-underline">
+      <RouterLink class="no-underline" to="/">My Workouts</RouterLink>
+      <RouterLink  to="/signup">Sign Up</RouterLink>
+    </nav>
+  </div>
+</template>
