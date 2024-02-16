4f07c16d6fcec4e6e520b94cc34b1ee78a03d222
Guilherme Martins Valente <guimartins.m.v@gmail.com>
2024-02-16 20:14:58 -0300

    server file added

---
+++ workout-server/src/main/java/com/api/workoutserver/WorkoutServerApplication.java
@@ -0,0 +1,16 @@
+package com.api.workoutserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutServerApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutServerApplication.class, args);
+	}
+
+	
+
+}
